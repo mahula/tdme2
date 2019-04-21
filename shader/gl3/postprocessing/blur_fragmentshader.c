ff388f9a1839bf89ae8c1d4f171e075634a71ae1
mahula <lenzmath@gmail.com>
2019-04-21 21:07:49 +0200

    Revert "shader/gl3/postprocessing/blur: Synched shader with current ASW shader code"
    This reverts commit aeeba69345f86b8ace636d8aa546115a424dc232.

--- shader/gl3/postprocessing/blur_fragmentshader.c
+++ shader/gl3/postprocessing/blur_fragmentshader.c
@@ -1,11 +1,7 @@
 #version 330
 
-#define MAP_LOOKUPS_NEAR	4
+#define MAP_LOOKUPS_NEAR		4
 #define MAP_LOOKUPS_FAR		8
-
-#define DISTANCE_NEAR		0.96
-#define DISTANCE_FAR		0.98
-#define DISTANCE_MAX		0.985
 
 // uniforms
 uniform sampler2D colorBufferTextureUnit;
@@ -22,26 +18,11 @@ out vec4 outColor;
 // main
 void main (void) {
 	float depth = texture(depthBufferTextureUnit, vsFragTextureUV).r;
-	vec4 originalColor = texture(colorBufferTextureUnit, vsFragTextureUV);
-	vec4 blurredColor = vec4(0.0, 0.0, 0.0, 0.0);
-	outColor = originalColor;
-	if (depth == 0.0) {
-		for (int y = 0; y < MAP_LOOKUPS_FAR; y++)
-		for (int x = 0; x < MAP_LOOKUPS_FAR; x++) {
-			if (texture(
-				depthBufferTextureUnit,
-				vsFragTextureUV.xy +
-					vec2(
-						(-MAP_LOOKUPS_FAR / 2.0 + 0.5 + x) * bufferTexturePixelWidth,
-						(-MAP_LOOKUPS_FAR / 2.0 + 0.5 + y) * bufferTexturePixelHeight
-					)
-			).r == 0.0) {
-				depth = 1.0;
-			}
-		}
-	}
-	if (depth > DISTANCE_NEAR) {
-		float intensity = clamp((depth - DISTANCE_NEAR) * 1.0 / (DISTANCE_FAR - DISTANCE_NEAR), 0.0, 1.0);
+	vec3 originalColor = texture(colorBufferTextureUnit, vsFragTextureUV).rgb;
+	vec3 blurredColor = vec3(0.0, 0.0, 0.0);
+	outColor = vec4(originalColor, 1.0);
+	if (depth > 0.96) {
+		float intensity = clamp((depth - 0.96) * 1.0 / (0.98 - 0.96), 0.0, 1.0);
 		for (int y = 0; y < MAP_LOOKUPS_NEAR; y++)
 		for (int x = 0; x < MAP_LOOKUPS_NEAR; x++) {
 			blurredColor+= texture(
@@ -51,17 +32,17 @@ void main (void) {
 						(-MAP_LOOKUPS_NEAR / 2.0 + 0.5 + x) * bufferTexturePixelWidth,
 						(-MAP_LOOKUPS_NEAR / 2.0 + 0.5 + y) * bufferTexturePixelHeight
 					)
-			);
+			).rgb;
 		}
 		blurredColor/= MAP_LOOKUPS_NEAR * MAP_LOOKUPS_NEAR;
 		blurredColor*= intensity;
 		blurredColor+= originalColor * (1.0 - intensity);
-		outColor = blurredColor;
+		outColor = vec4(blurredColor, 1.0);
 	}
-	if (depth > DISTANCE_FAR) {
+	if (depth > 0.98) {
 		originalColor = blurredColor;
-		blurredColor = vec4(0.0, 0.0, 0.0, 0.0);
-		float intensity = clamp((depth - DISTANCE_FAR) * 1.0 / (DISTANCE_MAX - DISTANCE_FAR), 0.0, 1.0);
+		blurredColor = vec3(0.0, 0.0, 0.0);
+		float intensity = clamp((depth - 0.98) * 1.0 / (0.985 - 0.98), 0.0, 1.0);
 		for (int y = 0; y < MAP_LOOKUPS_FAR; y++)
 		for (int x = 0; x < MAP_LOOKUPS_FAR; x++) {
 			blurredColor+= texture(
@@ -71,12 +52,12 @@ void main (void) {
 						(-MAP_LOOKUPS_FAR / 2.0 + 0.5 + x) * bufferTexturePixelWidth,
 						(-MAP_LOOKUPS_FAR / 2.0 + 0.5 + y) * bufferTexturePixelHeight
 					)
-			);
+			).rgb;
 		}
 		blurredColor/= MAP_LOOKUPS_FAR * MAP_LOOKUPS_FAR;
 		blurredColor*= intensity;
 		blurredColor+= originalColor * (1.0 - intensity);
-		outColor = blurredColor;
+		outColor = vec4(blurredColor, 1.0);
 	}
 	gl_FragDepth = depth;
 }
