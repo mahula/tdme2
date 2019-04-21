27aba6d95129bace698af9d1316ca1a0c97e630e
mahula <lenzmath@gmail.com>
2019-04-21 21:07:49 +0200

    Revert "post-processing: ssao: do not use mip map on rnd texture"
    This reverts commit 55b469c6dd77ffacf81c5a2b8171d90c5e3e2844.

--- src/tdme/engine/subsystems/postprocessing/PostProcessingShaderSSAOMapImplementation.cpp
+++ src/tdme/engine/subsystems/postprocessing/PostProcessingShaderSSAOMapImplementation.cpp
@@ -3,7 +3,6 @@
 
 #include <tdme/engine/Engine.h>
 #include <tdme/engine/fileio/textures/TextureLoader.h>
-#include <tdme/engine/fileio/textures/Texture.h>
 #include <tdme/engine/subsystems/manager/TextureManager.h>
 #include <tdme/engine/subsystems/postprocessing/PostProcessingShaderBaseImplementation.h>
 #include <tdme/engine/subsystems/postprocessing/PostProcessingShaderSSAOMapImplementation.h>
@@ -15,7 +14,6 @@ using std::string;
 using std::to_string;
 
 using tdme::engine::Engine;
-using tdme::engine::fileio::textures::Texture;
 using tdme::engine::fileio::textures::TextureLoader;
 using tdme::engine::subsystems::manager::TextureManager;
 using tdme::engine::subsystems::postprocessing::PostProcessingShaderSSAOMapImplementation;
@@ -68,9 +66,7 @@ void PostProcessingShaderSSAOMapImplementation::initialize()
 		initialized = false;
 		return;
 	}
-	auto randomTexture = TextureLoader::loadTexture("resources/textures", "random.png");
-	randomTexture->setUseMipMap(false);
-	randomTextureId = Engine::getInstance()->getTextureManager()->addTexture(randomTexture);
+	randomTextureId = Engine::getInstance()->getTextureManager()->addTexture(TextureLoader::loadTexture("resources/textures", "random.png"));
 }
 
 void PostProcessingShaderSSAOMapImplementation::useProgram() {
