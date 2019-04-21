2ac162a78786c0b6a6e9465d687a7a65f36a14fe
mahula <lenzmath@gmail.com>
2019-04-21 21:07:49 +0200

    Revert "engine/physics/World: small bug fix when shutting down or resetting physics engine"
    This reverts commit aad089ba061d901ac10651a46522445fb8b800bb.

--- src/tdme/engine/physics/World.cpp
+++ src/tdme/engine/physics/World.cpp
@@ -74,8 +74,7 @@ World::~World()
 
 void World::reset()
 {
-	auto _bodies = bodies;
-	for (auto body: _bodies) removeBody(body->getId());
+	for (auto body: bodies) removeBody(body->getId());
 	bodies.clear();
 	rigidBodiesDynamic.clear();
 	bodiesById.clear();
